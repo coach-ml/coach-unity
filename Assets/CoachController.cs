f538164d214151bf716974c4a523802a7afdf85a
Loren Kuich <loren.jk3@gmail.com>
2019-07-08 12:15:59 -0700

    Worked on barricuda impl

--- Assets/CoachController.cs
+++ Assets/CoachController.cs
@@ -7,27 +7,37 @@ using Barracuda;
 public class CoachController : MonoBehaviour
 {
     public Webcam Webcam;
+    private Dictionary<string, Tensor> Inputs { get; set; }
+    private IWorker Worker { get; set; }
+    private Tensor Output { get; set; }
 
     public void TakePhoto()
     {
         Texture2D image = Webcam.GetPhoto();
         Tensor imageTensor = new Tensor(image);
+        Inputs.Add("import/lambda_input_input", imageTensor);
 
-        // Load barricuda model
-        var model = ModelLoader.Load("flowers.bytes");
-        var worker = BarracudaWorkerFactory.CreateWorker(BarracudaWorkerFactory.Type.ComputePrecompiled, model);
+        // Await execution
+        Worker.Execute(Inputs);
 
-        var inputs = new Dictionary<string, Tensor>();
-        inputs.Add("myinput", imageTensor);
-
-        worker.Execute(inputs);
-        
+        // Get the output
+        Output = Worker.Fetch("import/softmax_input/Softmax");
+        Debug.Log(Output[0]);
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        // Load the model and spawn the worker
+        var model = ModelLoader.LoadFromStreamingAssets("flowers.bytes");
+        Worker = BarracudaWorkerFactory.CreateWorker(BarracudaWorkerFactory.Type.Compute, model);
         
+        Inputs = new Dictionary<string, Tensor>();
+    }
+
+    void Destroy() {
+        Output.Dispose();
+        Worker.Dispose();
     }
 
     // Update is called once per frame
