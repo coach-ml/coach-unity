6e945dbaaeec95eb3b03a865923d70b7bff6053d
Loren Kuich <lkuich@agentsofdiscovery.com>
2019-07-22 01:11:28 -0700

    Porting .NET standard SDK to Unity/Barracuda

--- Assets/Coach.cs
+++ Assets/Coach.cs
@@ -6,26 +6,166 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using Barracuda;
 
 namespace Coach
 {
-    public class CoachModel
+    public struct ImageDims
     {
-        public CoachModel()
+        public int InputSize { get; set; }
+        public int ImageMean { get; set; }
+        public float ImageStd { get; set; }
+
+        public ImageDims(int inputSize, int imageMean, float imageStd)
         {
-
+            this.InputSize = inputSize;
+            this.ImageMean = imageMean;
+            this.ImageStd = imageStd;
         }
-
     }
 
-    public struct Model
+    public static class ImageUtil
+    {
+        public static Texture2D TextureFromFile(string filePath)
+        {
+            return TextureFromBytes(File.ReadAllBytes(filePath));
+        }
+
+        public static Texture2D TextureFromBytes(byte[] input)
+        {
+            Texture2D tex = new Texture2D(0, 0);
+            tex.LoadImage(input); //..this will auto-resize the texture dimensions.
+
+            return tex;
+        }
+
+        public static Tensor TensorFromTexture(Texture2D image, ImageDims dims)
+        {
+            Tensor imageTensor = new Tensor(image);
+            return imageTensor.Reshape(
+                new TensorShape(1, dims.InputSize, dims.InputSize, 3)
+            );
+        }
+    }
+
+    public class CoachResult
+    {
+        public List<LabelProbability> Results { get; private set; }
+        public List<LabelProbability> SortedResults { get; private set; }
+
+        public CoachResult(string[] labels, Tensor output)
+        {
+            for (var i = 0; i < labels.Length; i++)
+            {
+                string label = labels[i];
+                float probability = output[i];
+
+                Results.Add(new LabelProbability()
+                {
+                    Label = label,
+                    Confidence = probability
+                });
+            }
+            SortedResults = Results.OrderByDescending(r => r.Confidence).ToList();
+        }
+
+        public LabelProbability Best()
+        {
+            return SortedResults.FirstOrDefault();
+        }
+
+        public LabelProbability Worst()
+        {
+            return SortedResults.LastOrDefault();
+        }
+    }
+
+    public struct LabelProbability
+    {
+        public string Label { get; set; }
+        public float Confidence { get; set; }
+    }
+
+    public class CoachModel
+    {
+        private readonly float COACH_VERSION = 1f;
+
+        private string[] Labels { get; set; }
+        private ImageDims ImageDims { get; set; }
+
+        private IWorker Worker { get; set; }
+
+        public CoachModel(Model model, string[] labels, string module, float coachVersion)
+        {
+            if (COACH_VERSION != coachVersion)
+            {
+                throw new Exception($"Coach model v{coachVersion} incompatible with SDK version {COACH_VERSION}");
+            }
+
+            Worker = BarracudaWorkerFactory.CreateWorker(BarracudaWorkerFactory.Type.Compute, model);
+
+            this.Labels = labels;
+
+            int size = int.Parse(module.Substring(module.Length - 3, 3));
+            // TODO: Normalization
+            this.ImageDims = new ImageDims(size, 0, 1);
+        }
+
+        private Tensor ReadTensorFromBytes(byte[] image)
+        {
+            var texture = ImageUtil.TextureFromBytes(image);
+            return ImageUtil.TensorFromTexture(texture, this.ImageDims);
+        }
+
+        private Tensor ReadTensorFromFile(string filePath)
+        {
+            var texture = ImageUtil.TextureFromFile(filePath);
+            return ImageUtil.TensorFromTexture(texture, this.ImageDims);
+        }
+
+        public CoachResult Predict(string image, string inputName = "input", string outputName = "output")
+        {
+            var imageTensor = ReadTensorFromFile(image);
+            return GetGraphResult(imageTensor, inputName, outputName);
+        }
+
+        public CoachResult Predict(byte[] image, string inputName = "input", string outputName = "output")
+        {
+            var imageTensor = ReadTensorFromBytes(image);
+            return GetGraphResult(imageTensor, inputName, outputName);
+        }
+
+        private CoachResult GetGraphResult(Tensor imageTensor, string inputName = "input", string outputName = "output")
+        {
+            var inputs = new Dictionary<string, Tensor>();
+            inputs.Add("input", imageTensor);
+
+            // Await execution
+            Worker.Execute(inputs);
+
+            // Get the output
+            var output = Worker.Fetch("output");
+            output.Dispose();
+
+            return new CoachResult(Labels, output);
+        }
+
+        public void CleanUp()
+        {
+            Worker.Dispose();
+        }
+    }
+
+    public struct ModelDef
     {
         public string name { get; set; }
         public string status { get; set; }
         public int version { get; set; }
         public string module { get; set; }
         public string[] labels { get; set; }
+        public float coachVersion { get; set; }
     }
 
     public class Profile
@@ -36,12 +176,16 @@ namespace Coach
         public string Bucket { get; set; }
 
         [JsonProperty("models")]
-        public Model[] Models { get; set; }
+        public ModelDef[] Models { get; set; }
+    }
+
+    public enum ModelType
+    {
+        Frozen, Unity, Mobile
     }
 
     public class CoachClient
     {
-
         public bool IsDebug { get; private set; }
         private Profile Profile { get; set; }
         private string ApiKey { get; set; }
@@ -51,10 +195,16 @@ namespace Coach
             this.IsDebug = isDebug;
         }
 
-        public async Task Login(string apiKey)
+        public async Task<CoachClient> Login(string apiKey)
         {
+            if (apiKey == String.Empty)
+            {
+                throw new Exception("Invalid API Key");
+            }
             this.ApiKey = apiKey;
             this.Profile = await GetProfile();
+
+            return this;
         }
 
         private bool IsAuthenticated()
@@ -62,65 +212,9 @@ namespace Coach
             return this.Profile != null;
         }
 
-        private Model ReadManifest(string path)
+        private ModelDef ReadManifest(string path)
         {
-            return JsonConvert.DeserializeObject<Model>(File.ReadAllText(path));
-        }
-
-        public async Task CacheModel(string name, string path = ".")
-        {
-            if (!IsAuthenticated())
-                throw new Exception("User is not authenticated");
-
-            // Create the target dir
-            if (!Directory.Exists(name))
-                Directory.CreateDirectory(name);
-
-            Model model = this.Profile.Models.Single(m => m.name == name);
-
-            int profileVersion = model.version;
-            string profileManifest = $"{path}/{name}/manifest.json";
-
-            if (File.Exists(profileManifest))
-            {
-                // Load existing model manifest
-                Model manifest = ReadManifest(profileManifest);
-                int manifestVersion = manifest.version;
-
-                if (profileVersion == manifestVersion)
-                {
-                    if (this.IsDebug)
-                    {
-                        Console.WriteLine("Version match, skipping model download");
-                    }
-
-                    return;
-                }
-            }
-            else
-            {
-                var json = JsonConvert.SerializeObject(model);
-                File.WriteAllText(profileManifest, json);
-            }
-
-            var baseUrl = $"https://la41byvnkj.execute-api.us-east-1.amazonaws.com/prod/{this.Profile.Bucket}/model-bin?object=trained/{name}/{profileVersion}/model";
-
-            var modelFile = "frozen.pb";
-            var modelUrl = $"{baseUrl}/{modelFile}";
-
-
-            var request = new HttpClient();
-            request.DefaultRequestHeaders.Add("X-Api-Key", this.ApiKey);
-            request.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
-
-            var requestMessage = new HttpRequestMessage(HttpMethod.Get, modelUrl);
-            requestMessage.Content = new StringContent(String.Empty, Encoding.UTF8, "application/octet-stream");
-
-            var response = await request.SendAsync(requestMessage);
-            response.EnsureSuccessStatusCode();
-
-            byte[] modelBytes = await response.Content.ReadAsByteArrayAsync();
-            File.WriteAllBytes($"{path}/{name}/{modelFile}", modelBytes);
+            return JsonConvert.DeserializeObject<ModelDef>(File.ReadAllText(path));
         }
 
         private async Task<Profile> GetProfile()
@@ -139,19 +233,83 @@ namespace Coach
             return profile;
         }
 
+        public async Task CacheModel(string modelName, string path = ".", bool skipMatch = true, ModelType modelType = ModelType.Frozen)
+        {
+            if (!IsAuthenticated())
+                throw new Exception("User is not authenticated");
+
+            ModelDef model = this.Profile.Models.Single(m => m.name == modelName);
+            int version = model.version;
+
+            string modelDir = Path.Combine(path, modelName);
+            string profileManifest = Path.Combine(modelDir, "manifest.json");
+
+            if (File.Exists(profileManifest))
+            {
+                // Load existing model manifest
+                ModelDef manifest = ReadManifest(profileManifest);
+
+                int manifestVersion = manifest.version;
+                int profileVersion = model.version;
+
+                if (profileVersion == manifestVersion && skipMatch)
+                {
+                    if (this.IsDebug)
+                    {
+                        Console.WriteLine("Version match, skipping model download");
+                    }
+                    return;
+                }
+            }
+            else if (!Directory.Exists(modelDir))
+            {
+                Directory.CreateDirectory(modelDir);
+            }
+
+            // Write downloaded manifest
+            var json = JsonConvert.SerializeObject(model);
+            File.WriteAllText(profileManifest, json);
+
+            var baseUrl = $"https://la41byvnkj.execute-api.us-east-1.amazonaws.com/prod/{this.Profile.Bucket}/model-bin?object=trained/{modelName}/{version}/model";
+
+            var modelFile = "frozen.pb";
+            var modelUrl = $"{baseUrl}/{modelFile}";
+
+            var request = new HttpClient();
+            request.DefaultRequestHeaders.Add("X-Api-Key", this.ApiKey);
+            request.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, modelUrl);
+            requestMessage.Content = new StringContent(String.Empty, Encoding.UTF8, "application/octet-stream");
+
+            var response = await request.SendAsync(requestMessage);
+            response.EnsureSuccessStatusCode();
+
+            byte[] modelBytes = await response.Content.ReadAsByteArrayAsync();
+            File.WriteAllBytes(Path.Combine(path, modelName, modelFile), modelBytes);
+        }
+
         public CoachModel GetModel(string path)
         {
-            var graphPath = $"{path}/frozen.pb";
-            var labelPath = $"{path}/manifest.json";
+            var modelPath = Path.Combine(path, "unity.bytes");
+            var labelPath = Path.Combine(path, "manifest.json");
 
-            // Load the graphdef
+            // Load the model
+            Model model = ModelLoader.LoadFromStreamingAssets(modelPath);
 
             var manifest = ReadManifest(labelPath);
 
             string[] labels = manifest.labels;
             string baseModule = manifest.module;
+            float coachVersion = manifest.coachVersion;
 
-            return new CoachModel();
+            return new CoachModel(model, labels, baseModule, coachVersion);
+        }
+
+        public async Task<CoachModel> GetModelRemote(string modelName, string path = ".")
+        {
+            await CacheModel(modelName, path);
+            return GetModel(Path.Combine(modelName, path));
         }
     }
 }
