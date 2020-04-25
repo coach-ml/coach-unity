98d4e7880584b38fea13b8f2034ba578e46ff9e0
Loren Kuich <lkuich@agentsofdiscovery.com>
2020-04-24 18:34:25 -0700

    Updated to Barracuda 0.6.3

---
+++ Assets/Scenes/Example.cs
@@ -0,0 +1,32 @@
+﻿using Coach;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Example : MonoBehaviour
+{
+    private CoachModel model;
+    private Texture2D texture;
+
+    public RawImage image;
+    public Text resultLabel;
+    
+    async void Start()
+    {
+        var coach = new CoachClient(isDebug: true);
+        coach = await coach.Login("A2botdrxAn68aZh8Twwwt2sPBJdCfH3zO02QDMt0");
+        model = await coach.GetModelRemote("flowers");
+
+        texture = image.texture as Texture2D;
+    }
+
+    private void OnDestroy()
+    {
+        model.CleanUp();
+    }
+
+    public void evaluateRose()
+    {
+        var result = model.Predict(this.texture).Best();
+        resultLabel.text = $"{result.Label}: {result.Confidence.ToString()}";
+    }
+}
